6aba048fcc9dea2633769b2fccaa608eff12e49d
riyagoyal666 <98308048+riyagoyal666@users.noreply.github.com>
2024-07-26 09:35:26 +0530

    Update Home.jsx

--- noticeBoard/src/Pages/Home.jsx
+++ noticeBoard/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-// src/pages/HomePage.jsx
+
 import React from 'react';
 import GoogleSlides from '../Components/GSlides';
 import PomodoroTimer from '../Components/PTimer';
@@ -6,17 +6,25 @@ import Clock from '../Components/Clock';
 import DailyQuote from '../Components/Quote';
 // import HorizontalScrollBar from '../Components/HBar';
 import Example from '../Components/HBarUse';
+import Announcements from '../Components/Announcements';
+import ChatGPTBox from '../Components/ChatGPTbox';
 
 const HomePage = () => (
   <div>
-    <h1>Digital Notice Board</h1>
+    <div class="bg-white p-8 rounded-lg shadow-lg text-center">
+        <h1 class="text-4xl font-bold text-gray-900">Digital Notice Board</h1>
+    </div>
     <Example/>
+    <Announcements/>
     <Clock/>
     <DailyQuote/>
+    <ChatGPTBox/>
 
     <GoogleSlides />
-    {/* Render other optional widgets */}
+    
+ 
   </div>
 );
 
 export default HomePage;
+
