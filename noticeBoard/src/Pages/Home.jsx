cf498be0f045165f158aafe28b80e6369a24e764
dishapurwar <114928200+dishapurwar@users.noreply.github.com>
2024-07-26 11:37:37 +0530

    Update Home.jsx

--- noticeBoard/src/Pages/Home.jsx
+++ noticeBoard/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-
+// src/components/HomePage.jsx
 import React from 'react';
 import GoogleSlides from '../Components/GSlides';
 import PomodoroTimer from '../Components/PTimer';
@@ -11,25 +11,34 @@ import ChatGPTBox from '../Components/ChatGPTbox';
 import MusicWidgets from '../Components/MusicWidget';
 import GoogleMeetWidget from '../Components/GMeet';
 import HealthTrackerWidget from '../Components/HealthTracking';
+
 const HomePage = () => (
-  <div>
-    <div class="bg-white p-8 rounded-lg shadow-lg text-center">
-        <h1 class="text-4xl font-bold text-gray-900">Digital Notice Board</h1>
+  <div className="homepage-container p-10">
+    <div className="header bg-white p-8 rounded-lg shadow-lg text-center mb-8">
+        <h1 className="text-4xl font-bold text-gray-900">Digital Notice Board</h1>
     </div>
-    <Example/>
-    <Announcements/>
-    <Clock/>
-    <DailyQuote/>
-    <MusicWidgets/>
-    <HealthTrackerWidget/>
-    <GoogleMeetWidget/>
-    <ChatGPTBox/>
     
-    <GoogleSlides />
-    
- 
+    <Announcements />
+   
+    <div className="dashboard-container flex flex-wrap space-x-15 space-y-0 items-start gap-20 mb-8">
+      <div className='clock-timer flex-row'>
+      <Clock />
+      <DailyQuote />
+      </div>
+      <MusicWidgets />
+      <GoogleMeetWidget />
+      <HealthTrackerWidget />
+    </div>
+
+
+
+    <div className="dashboard-container flex flex-row space-x-15 space-y-0 items-start gap-4 mb-8 margin-left 15">
+      
+      <ChatGPTBox />
+      <GoogleSlides />
+
+    </div>
   </div>
 );
 
 export default HomePage;
-
